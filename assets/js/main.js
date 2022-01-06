a2697e9097f345380f199075677b514d65673540
trgomez <88842062+trrgomez@users.noreply.github.com>
2022-01-06 14:55:16 -0600

    updated and refactored code in index.html and main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
-function showHeader(){
-    document.getElementById('header').classList.toggle('active')
-}
+let menu = document.querySelector('#menu')
+let header = document.querySelector('#header')
+
+// toggles the menu button
+menu.addEventListener('click', function(){
+    menu.classList.toggle('fa-times')
+    header.classList.toggle('active')
+})
